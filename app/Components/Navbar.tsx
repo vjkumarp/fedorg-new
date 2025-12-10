84890fed871c052ff858647862357f1dedaacbda
Vijay Kumar P <iamvj1993kumar@gmail.com>
2025-12-10 08:36:36 +0530

    Changes on Adding CSS

--- app/Components/Navbar.tsx
+++ app/Components/Navbar.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-sky-500">Hello world!</h1>
+    </div>
+  );
 }
 
 export default Navbar;
