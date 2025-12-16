9d8e7d0d0e866bb76bbd05eccfd5b5a51c1a8f8b
Vijay Kumar P <iamvj1993kumar@gmail.com>
2025-12-16 20:20:23 +0530

    Required Changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 import ComingSoon from "./Components/ComingSoon/ComingSoon";
+import Navbar from "./Components/Navbar/Navbar";
+import "./globals.css";
 
 export default function Home() {
   return (
     <>
+      <Navbar />
       <ComingSoon />
     </>
   );
